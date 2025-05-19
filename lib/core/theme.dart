10c26eb04c82c56c1a7b948be703b87f8693c60f
luuisaaaa <luisacrivo2003@gmail.com>
2025-05-19 12:22:53 +0200

    .

---
+++ lib/core/theme.dart
@@ -0,0 +1,8 @@
+ThemeData myAppTheme = ThemeData(
+  primarySwatch: Colors.indigo,
+  scaffoldBackgroundColor: Colors.grey[100],
+  textTheme: const TextTheme(
+    titleLarge: TextStyle(fontWeight: FontWeight.bold, color: Colors.black87),
+    bodyMedium: TextStyle(color: Colors.black54),
+  ),
+);
