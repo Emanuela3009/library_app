585d41d6609595b6ac61af58d2ca947eda760e2c
luuisaaaa <luisacrivo2003@gmail.com>
2025-05-19 13:37:19 +0200

    theme

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -1,8 +1,30 @@
-ThemeData myAppTheme = ThemeData(
-  primarySwatch: Colors.indigo,
-  scaffoldBackgroundColor: Colors.grey[100],
-  textTheme: const TextTheme(
-    titleLarge: TextStyle(fontWeight: FontWeight.bold, color: Colors.black87),
-    bodyMedium: TextStyle(color: Colors.black54),
+import 'package:flutter/material.dart';
+
+
+final ThemeData myAppTheme = ThemeData(
+  useMaterial3: true, // se vuoi usare Material Design 3
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+  scaffoldBackgroundColor: Colors.white,
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Colors.indigo,
+    foregroundColor: Colors.white,
+    elevation: 2,
   ),
-);
+  textTheme: const TextTheme(
+    titleLarge: TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 16,
+      color: Colors.black87,
+    ),
+  ),
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+    selectedItemColor: Colors.indigo,
+    unselectedItemColor: Colors.grey,
+    backgroundColor: Colors.white,
+    elevation: 8,
+  ),
+); 
