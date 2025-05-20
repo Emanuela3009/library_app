29ad7a8d996c48a68763a0568d418d5939281e8e
Emanuela3009 <graziusoemanuela@gmail.com>
2025-05-20 14:00:56 +0200

    Aggiunta immagini, categorie e gestione form

---
+++ lib/data/fake_categories.dart
@@ -0,0 +1,20 @@
+import '../models/category.dart';
+
+final List<Category> fakeCategories = [
+  Category(
+    name: 'Romance',
+    bookCount: 3,
+    imagePath: 'assets/category/romance.jpg',
+  ),
+  Category(
+    name: 'Fantasy',
+    bookCount: 1,
+    imagePath: 'assets/category/fantasy.jpg',
+  ),
+  Category(
+    name: 'Thriller', 
+    bookCount: 4, 
+    imagePath: 'assets/category/thriller.jpg',
+  ),
+];
+
--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,6 +1,7 @@
 class Category {
   final String name;
   final int bookCount;
+  final String imagePath;
 
-  Category({required this.name, required this.bookCount});
+  Category({required this.name, required this.bookCount, required this.imagePath});
 }
